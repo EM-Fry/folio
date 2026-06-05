c54fa429bfa10aa20d3206e98563496334d58d4a
EM-Fry <122057972+EM-Fry@users.noreply.github.com>
2026-06-05 16:04:23 +1000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+// const driftingThing = document.querySelector('.cloud');
+
+// function restartAnimation() {
+//   driftingThing.style.animation = 'none';
+//   driftingThing.offsetHeight; // Force a reflow
+//   driftingThing.style.animation = '';
+// }
+
+// window.addEventListener('resize', restartAnimation);
